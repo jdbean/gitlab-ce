476c7b353ab64b05b460fa0fd43ecf5bd32f8b0b
Francisco Javier López <fjlopez@gitlab.com>
2018-07-31 16:47:30 -0400

    Added check to avoid collections

--- lib/api/helpers/headers_helpers.rb
+++ lib/api/helpers/headers_helpers.rb
@@ -3,6 +3,8 @@ module API
     module HeadersHelpers
       def set_http_headers(header_data)
         header_data.each do |key, value|
+          next if value.is_a?(Enumerable)
+
           header "X-Gitlab-#{key.to_s.split('_').collect(&:capitalize).join('-')}", value.to_s
         end
       end
