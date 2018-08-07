a667220182a66e5ccdc2dc18fe550f71b2c648a0
Rémy Coutable <remy@rymai.me>
2018-08-07 17:06:31 +0200

    Improve QA scenarios contexts & descriptions consistency
    Signed-off-by: Rémy Coutable <remy@rymai.me>

--- qa/qa/specs/features/browser/3_create/merge_request/merge_merge_request_from_fork_spec.rb
+++ qa/qa/specs/features/browser/3_create/merge_request/merge_merge_request_from_fork_spec.rb
@@ -1,23 +1,27 @@
+# frozen_string_literal: true
+
 module QA
-  describe 'Project fork', :core do
-    it 'can submit merge requests to upstream master' do
-      Runtime::Browser.visit(:gitlab, Page::Main::Login)
-      Page::Main::Login.act { sign_in_using_credentials }
+  context :create, :core do
+    describe 'Merge request creation from fork' do
+      it 'user forks a project, submits a merge request and maintainer merges it' do
+        Runtime::Browser.visit(:gitlab, Page::Main::Login)
+        Page::Main::Login.act { sign_in_using_credentials }
 
-      merge_request = Factory::Resource::MergeRequestFromFork.fabricate! do |merge_request|
-        merge_request.fork_branch = 'feature-branch'
+        merge_request = Factory::Resource::MergeRequestFromFork.fabricate! do |merge_request|
+          merge_request.fork_branch = 'feature-branch'
+        end
+
+        Page::Menu::Main.act { sign_out }
+        Page::Main::Login.act do
+          switch_to_sign_in_tab
+          sign_in_using_credentials
+        end
+
+        merge_request.visit!
+        Page::MergeRequest::Show.act { merge! }
+
+        expect(page).to have_content('The changes were merged')
       end
-
-      Page::Menu::Main.act { sign_out }
-      Page::Main::Login.act do
-        switch_to_sign_in_tab
-        sign_in_using_credentials
-      end
-
-      merge_request.visit!
-      Page::MergeRequest::Show.act { merge! }
-
-      expect(page).to have_content('The changes were merged')
     end
   end
 end
