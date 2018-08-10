826a2393f877327cf1613798b328ba7306c841b0
Luke Bennett <lukeeeebennettplus@gmail.com>
2018-08-10 13:32:49 +0100

    Implement more advanced dirty submit

--- app/assets/javascripts/dirty_submit.js
+++ app/assets/javascripts/dirty_submit.js
@@ -1,12 +1,51 @@
+function disableSubmitIfDirty(inputs, submit) {
+  const dirtySubmit = submit;
+  dirtySubmit.disabled = !inputs.some(input => input.dataset.isDirty === 'true');
+}
+
+function isCheckable(input) {
+  return input.type === 'checkbox' || input.type === 'radio';
+}
+
+function currentValue(input) {
+  return isCheckable(input) ? input.checked.toString() : input.value;
+}
+
+function setIsDirty(input) {
+  const dirtySubmitInput = input;
+  dirtySubmitInput.dataset.isDirty = input.dataset.dirtySubmitOriginalValue !== currentValue(input);
+}
+
 export default function initDirtySubmit(form) {
-  const submit = form.querySelector('.js-dirty-submit');
+  const inputs = Array.prototype.slice.call(form.querySelectorAll('input, textarea'));
+  const submit = inputs.find(input => input.classList.contains('js-dirty-submit'));
+
   submit.disabled = true;
 
-  function enableSubmit() {
-    submit.disabled = false;
+  inputs.forEach(input => {
+    const dirtySubmitInput = input;
 
-    this.removeEventListener('input', enableSubmit);
-  }
+    dirtySubmitInput.dataset.dirtySubmitOriginalValue = currentValue(input);
+    dirtySubmitInput.dataset.isDirty = false;
+  });
 
-  form.addEventListener('input', enableSubmit);
+  form.addEventListener('input', event => {
+    const input = event.target;
+    if (!input.dataset.dirtySubmitOriginalValue) return;
+
+    if (input.type === 'radio') {
+      const relatedInputs = Array.prototype.slice.call(
+        form.querySelectorAll(`input[type=radio][name="${input.name}"`),
+      );
+      relatedInputs.forEach(setIsDirty);
+    } else {
+      setIsDirty(input);
+    }
+
+    disableSubmitIfDirty(inputs, submit);
+  });
+}
+
+export function initDirtySubmitMulti(forms) {
+  forms.forEach(initDirtySubmit);
 }
--- app/assets/javascripts/pages/groups/edit/index.js
+++ app/assets/javascripts/pages/groups/edit/index.js
@@ -2,7 +2,7 @@ import groupAvatar from '~/group_avatar';
 import TransferDropdown from '~/groups/transfer_dropdown';
 import initConfirmDangerModal from '~/confirm_danger_modal';
 import initSettingsPanels from '~/settings_panels';
-import initDirtySubmit from '~/dirty_submit';
+import { initDirtySubmitMulti } from '~/dirty_submit';
 
 document.addEventListener('DOMContentLoaded', () => {
   groupAvatar();
@@ -13,5 +13,8 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('DOMContentLoaded', () => {
   // Initialize expandable settings panels
   initSettingsPanels();
-  initDirtySubmit(document.querySelector('.js-general-settings-form'));
+  initDirtySubmitMulti([
+    document.querySelector('.js-general-settings-form'),
+    document.querySelector('.js-general-permissions-form'),
+  ]);
 });
