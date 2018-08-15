47902eecb61afe878d6f65482c13d557ea7f27b4
Thong Kuah <tkuah@gitlab.com>
2018-08-16 10:38:42 +1200

    Refactor ServiceAccount to #generate an approriate Kubeclient resource

--- lib/gitlab/kubernetes/service_account.rb
+++ lib/gitlab/kubernetes/service_account.rb
@@ -3,30 +3,15 @@
 module Gitlab
   module Kubernetes
     class ServiceAccount
-      attr_reader :name, :namespace_name, :client
+      attr_reader :name, :namespace_name
 
-      def initialize(name, namespace_name, client)
+      def initialize(name, namespace_name)
         @name = name
         @namespace_name = namespace_name
-        @client = client
       end
 
-      def create!
-        resource = ::Kubeclient::Resource.new(metadata: metadata)
-
-        client.create_service_account(resource)
-      end
-
-      def exists?
-        client.get_service_account(name, namespace_name)
-      rescue ::Kubeclient::HttpError => ke
-        raise ke unless ke.error_code == 404
-
-        false
-      end
-
-      def ensure_exists!
-        exists? || create!
+      def generate
+        ::Kubeclient::Resource.new(metadata: metadata)
       end
 
       private
