71297e51b62c0ba01ed6cb625f3618167448bbfd
Brett Walker <bwalker@gitlab.com>
2018-08-16 20:42:07 -0500

    add SpacedLinkFilter for wiki links with spaces

---
+++ lib/banzai/filter/spaced_link_filter.rb
@@ -0,0 +1,72 @@
+require 'uri'
+
+module Banzai
+  module Filter
+    # HTML Filter for markdown links with spaces in the URLs
+    #
+    # Based on Banzai::Filter::AutolinkFilter
+    #
+    # CommonMark does not allow spaces in the url portion of a link.
+    # For example, `[example](page slug)` is not valid.  However,
+    # in our wikis, we support (via RedCarpet) this type of link, allowing
+    # wiki pages to be easily linked bby their title.  This filter adds that functionality.
+    # The intent is for this to only be used in Wikis - in general, we want
+    # to adhere to CommonMark's spec.
+    #
+    class SpacedLinkFilter < HTML::Pipeline::Filter
+      include ActionView::Helpers::TagHelper
+
+      # Pattern to match a standard markdown link
+      #
+      # Rubular: http://rubular.com/r/z9EAHxYmKI
+      LINK_PATTERN = /\[([^\]]+)\]\(([^)"]+)(?: \"([^\"]+)\")?\)/
+
+      # Text matching LINK_PATTERN inside these elements will not be linked
+      IGNORE_PARENTS = %w(a code kbd pre script style).to_set
+
+      # The XPath query to use for finding text nodes to parse.
+      TEXT_QUERY = %Q(descendant-or-self::text()[
+        not(#{IGNORE_PARENTS.map { |p| "ancestor::#{p}" }.join(' or ')})
+        and contains(., ']\(')
+      ]).freeze
+
+      def call
+        return doc if context[:markdown_engine] == :redcarpet
+
+        doc.xpath(TEXT_QUERY).each do |node|
+          content = node.to_html
+
+          next unless content.match(LINK_PATTERN)
+
+          html = spaced_link_filter(content)
+
+          next if html == content
+
+          node.replace(html)
+        end
+
+        doc
+      end
+
+      private
+
+      def spaced_link_match(link)
+        match = LINK_PATTERN.match(link)
+        if match && match[1] && match[2]
+          # escape the spaces in the url so that it's a valid markdown link,
+          # then run it through the markdown processor again, let it do it's magic
+          new_link = "[#{match[1]}](#{match[2].gsub(' ', '%20')})"
+          Banzai::Filter::MarkdownFilter.new(new_link, context).call
+        else
+          link
+        end
+      end
+
+      def spaced_link_filter(text)
+        Gitlab::StringRegexMarker.new(CGI.unescapeHTML(text), text.html_safe).mark(LINK_PATTERN) do |link, left:, right:|
+          spaced_link_match(link)
+        end
+      end
+    end
+  end
+end
