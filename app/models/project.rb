2dd30fe3b6f886dbc366796473b8c947b3bdeacd
J.D. Bean <jonathandbean@gmail.com>
2018-09-07 22:34:14 -0400

    Removing trailing whitespace

--- app/models/project.rb
+++ app/models/project.rb
@@ -1171,7 +1171,7 @@ class Project < ActiveRecord::Base
 
   def license_template
     return unless license_key
-    
+
     templates = LicenseTemplateFinder.new.execute
     templates.find { |template| template.key == repository.license_key }
   end
